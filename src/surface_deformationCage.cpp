77da221635a775883e3b62cf332ef802c999a83c
Arnaud Bletterer <arnaud.bletterer@gmail.com>
2014-03-17 08:29:45 +0100

    Retour au premier calcul de coordonnées MVC 2D

--- src/surface_deformationCage.cpp
+++ src/surface_deformationCage.cpp
@@ -496,65 +496,65 @@ PFP2::REAL Surface_DeformationCage_Plugin::computeMVC2D(const PFP2::VEC3& pt, Da
     bool positiveAngle_prev = Geom::testOrientation2D(pt, c_prev, c) == Geom::LEFT;
     bool positiveAngle_next = Geom::testOrientation2D(pt, c, c_next) == Geom::LEFT;
 
-//    PFP2::VEC3 v = c - pt ;
-//    PFP2::VEC3 v_prev = c_prev - pt ;
-//    PFP2::VEC3 v_next = c_next - pt ;
+    PFP2::VEC3 v = c - pt ;
+    PFP2::VEC3 v_prev = c_prev - pt ;
+    PFP2::VEC3 v_next = c_next - pt ;
 
-//    PFP2::REAL B_prev = Geom::angle(v_prev, v);
-//    PFP2::REAL B_next = Geom::angle(v, v_next);
+    PFP2::REAL B_prev = Geom::angle(v_prev, v);
+    PFP2::REAL B_next = Geom::angle(v, v_next);
 
-//    if(!positiveAngle_prev)
-//    {
-//        B_prev *= -1;
-//    }
+    if(!positiveAngle_prev)
+    {
+        B_prev *= -1;
+    }
 
-//    if(!positiveAngle_next)
-//    {
-//        B_next *= -1;
-//    }
+    if(!positiveAngle_next)
+    {
+        B_next *= -1;
+    }
 
-//    res = (tan(B_prev/2.f) + (tan(B_next/2.f))) / v.norm();
+    res = (tan(B_prev/2.f) + (tan(B_next/2.f))) / v.norm();
 
-    const PFP2::VEC3 di_prev = c_prev - pt;
-    const PFP2::VEC3 di_next = c_next - pt;
+//    const PFP2::VEC3 di_prev = c_prev - pt;
+//    const PFP2::VEC3 di_next = c_next - pt;
 
-    const PFP2::REAL ri_prev = di_prev.norm();
-    const PFP2::REAL ri_next = di_next.norm();
+//    const PFP2::REAL ri_prev = di_prev.norm();
+//    const PFP2::REAL ri_next = di_next.norm();
 
     //res = sqrt( (2 * (ri_prev*ri_next - di_prev*di_next)) / ((ri_prev*ri + di_prev*di) * (ri*ri_next + di*di_next)) );
 
-    if(!positiveAngle_prev && positiveAngle_next)
-    {
-        res = - sqrt((ri_prev*ri_next - di_prev*di_next));
-    }
-    else if(!positiveAngle_next && positiveAngle_prev)
-    {
-        res = - sqrt((ri_prev*ri_next - di_prev*di_next));
-    }
-    else if(!positiveAngle_prev && !positiveAngle_next)
-    {
-        res = - sqrt((ri_prev*ri_next - di_prev*di_next));
-    }
-    else
-    {
-        res = sqrt((ri_prev*ri_next - di_prev*di_next));
-    }
+//    if(!positiveAngle_prev && positiveAngle_next)
+//    {
+//        res = - sqrt((ri_prev*ri_next - di_prev*di_next));
+//    }
+//    else if(!positiveAngle_next && positiveAngle_prev)
+//    {
+//        res = - sqrt((ri_prev*ri_next - di_prev*di_next));
+//    }
+//    else if(!positiveAngle_prev && !positiveAngle_next)
+//    {
+//        res = - sqrt((ri_prev*ri_next - di_prev*di_next));
+//    }
+//    else
+//    {
+//        res = sqrt((ri_prev*ri_next - di_prev*di_next));
+//    }
 
-    Traversor2FV<PFP2::MAP> trav_vert_face_cage(*cage, current);
+//    Traversor2FV<PFP2::MAP> trav_vert_face_cage(*cage, current);
 
-    for(Dart d = trav_vert_face_cage.begin(); d != trav_vert_face_cage.end(); d = trav_vert_face_cage.next())
-    {
-        PFP2::VEC3 dj = positionCage[d]-pt;
-        PFP2::VEC3 dj_next = positionCage[cage->phi1(d)]-pt;
+//    for(Dart d = trav_vert_face_cage.begin(); d != trav_vert_face_cage.end(); d = trav_vert_face_cage.next())
+//    {
+//        PFP2::VEC3 dj = positionCage[d]-pt;
+//        PFP2::VEC3 dj_next = positionCage[cage->phi1(d)]-pt;
 
-        PFP2::REAL rj = dj.norm();
-        PFP2::REAL rj_next = dj_next.norm();
+//        PFP2::REAL rj = dj.norm();
+//        PFP2::REAL rj_next = dj_next.norm();
 
-        if(d!=current && d!=previous)
-        {
-            res *= sqrt(rj*rj_next + dj*dj_next);
-        }
-    }
+//        if(d!=current && d!=previous)
+//        {
+//            res *= sqrt(rj*rj_next + dj*dj_next);
+//        }
+//    }
 
     return res;
 }
